096bb9a6474484e91c08a0cab248a880b159f25d
Facundo Poblete <facundo1792@gmail.com>
2019-10-02 20:23:54 -0300

    Fix extra claims update

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -43,7 +43,7 @@ export async function updateUser(user) {
       body: JSON.stringify({
         fullName: user.fullName,
         metadata: user.metadata,
-        extra_claims: user.extra_claims,
+        extraClaims: user.extraClaims,
       }),
       headers: { "Content-Type": "application/json", ...(await authHeader()) }
     });
